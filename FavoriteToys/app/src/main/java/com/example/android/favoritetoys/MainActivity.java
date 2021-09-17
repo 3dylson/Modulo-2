ce932be646ee296feea1f713e4518c79a582c88a
Edylson Frederico <Edylson_Frederico@outlook.com>
2021-09-17 11:58:20 +0100

    Start an Activity - Lesson 4: 6

--- FavoriteToys/app/src/main/java/com/example/android/favoritetoys/MainActivity.java
+++ FavoriteToys/app/src/main/java/com/example/android/favoritetoys/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.android.favoritetoys;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -22,6 +25,41 @@ public class MainActivity extends AppCompatActivity {
 
         mDoSomethingCoolButton = findViewById(R.id.b_do_something_cool);
         mNameEntry = findViewById(R.id.et_text_entry);
+
+        mDoSomethingCoolButton.setOnClickListener(new View.OnClickListener() {
+
+            /**
+             * The onClick method is triggered when this button (mDoSomethingCoolButton) is clicked.
+             *
+             * @param v The view that is clicked. In this case, it's mDoSomethingCoolButton.
+             */
+            @Override
+            public void onClick(View v) {
+                /*
+                 * Storing the Context in a variable in this case is redundant since we could have
+                 * just used "this" or "MainActivity.this" in the method call below. However, we
+                 * wanted to demonstrate what parameter we were using "MainActivity.this" for as
+                 * clear as possible.
+                 */
+                Context context = MainActivity.this;
+
+                /* This is the class that we want to start (and open) when the button is clicked. */
+                Class destinationActivity = ChildActivity.class;
+
+                /*
+                 * Here, we create the Intent that will start the Activity we specified above in
+                 * the destinationActivity variable. The constructor for an Intent also requires a
+                 * context, which we stored in the variable named "context".
+                 */
+                Intent startChildActivityIntent = new Intent(context, destinationActivity);
+
+                /*
+                 * Once the Intent has been created, we can use Activity's method, "startActivity"
+                 * to start the ChildActivity.
+                 */
+                startActivity(startChildActivityIntent);
+            }
+        });
 
     }
 }
