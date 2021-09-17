8f5907013d242bd5f81cedda3c0a12cf5ab44f38
Edylson Frederico <Edylson_Frederico@outlook.com>
2021-09-17 16:26:55 +0100

    Send Data - Lesson 4: 24

--- Sunshine/app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ Sunshine/app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -2,15 +2,31 @@ package com.example.android.sunshine;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
 
     private static final String FORECAST_SHARE_HASHTAG = " #SunshineApp";
 
+    private String mForecast;
+    private TextView mWeatherDisplay;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        mWeatherDisplay = findViewById(R.id.tv_display_weather);
+
+        Intent intentThatStartedThisActivity = getIntent();
+
+        if (intentThatStartedThisActivity != null) {
+            if (intentThatStartedThisActivity.hasExtra(Intent.EXTRA_TEXT)) {
+                mForecast = intentThatStartedThisActivity.getStringExtra(Intent.EXTRA_TEXT);
+                mWeatherDisplay.setText(mForecast);
+            }
+        }
     }
 }
